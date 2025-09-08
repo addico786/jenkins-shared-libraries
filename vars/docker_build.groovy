8b39084be4b7ff0c7cc6c6fffeca230400fb11e9
Adnan Khan <adnandelhi2004@gmail.com>
2025-09-08 17:02:13 +0530

    Update docker_build.groovy

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,5 +1,5 @@
 def call (String imagename, String imagetag, String path){
   echo "this is building the code"
-  sh "sudo  docker build -t ${imagetag}:${imagetag} ${path}"
+  sh "sudo  docker build -t ${imagename}:${imagetag} ${path}"
   echo "the code has been built sucessfully"
 }
